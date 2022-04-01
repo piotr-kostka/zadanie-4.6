23b4dd9da46f2bb20d4d436b1f75bdad9ba00eae
Piotr Kostka <piotrek.kostka94@gmail.com>
2022-04-01 20:38:48 +0200

    First Commit

--- src/main/java/com/kodilla/Zadanie46Application.java
+++ src/main/java/com/kodilla/Zadanie46Application.java
@@ -1,12 +1,17 @@
 package com.kodilla;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
 public class Zadanie46Application {
 
     public static void main(String[] args) {
+        SpringApplication.run(Zadanie46Application.class, args);
 
         Calculator calculator = new Calculator(10,5);
         System.out.println("Wynik dodawania to: " + calculator.add());
         System.out.println("Wynik odejmowania to: " + calculator.substract());
     }
-
 }
